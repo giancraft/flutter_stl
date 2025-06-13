69cd6bee2dc61a0e8e7e9762e92227a2aed8c912
giancraft <gianlucamk04@gmail.com>
2025-06-13 12:12:56 -0300

    add base do projeto

---
+++ android/app/src/main/kotlin/com/example/exibicao_stl_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.exibicao_stl_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_angle
+import video_player_avfoundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterAnglePlugin.register(with: registry.registrar(forPlugin: "FlutterAnglePlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
+}
